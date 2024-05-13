b0dd6fea99e88a84d3353b4715df64416cdc7dd1
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-05-13 15:27:24 +0300

    moved config.js to gitignore

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-const apiKey = import.meta.env.VITE_LASTFM_KEY;
-const sharedSecret = import.meta.env.VITE_SHARED_SECRET;
-const username = import.meta.env.VITE_USERNAME;
-const appName = import.meta.env.VITE_APPLICATION_NAME;
+const apiKey = 'dfa5ecd46c640751eb3a57f30ae610ce';
 
-export { apiKey, sharedSecret, username, appName };
+
+export { apiKey };
