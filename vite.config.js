775a77bfdcf0094810aea75d3d439342898765fe
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-06-29 17:40:33 +0300

    hide api

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
+import { defineConfig } from 'vite'
+import dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 export default defineConfig({
   define: {
-    'import.meta.env.VITE_LASTFM_KEY': JSON.stringify(process.env.VITE_LASTFM_KEY)
+    'import.meta.env': {
+      VITE_LASTFM_KEY: process.env.VITE_LASTFM_KEY
+    }
   }
-});
+})
