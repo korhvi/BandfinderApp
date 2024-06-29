468d869aeac70323e1618ccc996e2fb833cdee0e
Ville Korhonen <112542569+korhvi@users.noreply.github.com>
2024-06-29 17:35:23 +0300

    hide apikey

--- dist/assets/config-Dfv0kBNS.js
+++
@@ -1,1 +0,0 @@
-var e={VITE_LASTFM_KEY:"dfa5ecd46c640751eb3a57f30ae610ce",BASE_URL:"/",MODE:"production",DEV:!1,PROD:!0,SSR:!1};console.log("Environment Variables:",e);const a={apiKey:"dfa5ecd46c640751eb3a57f30ae610ce"};export{a as default};
